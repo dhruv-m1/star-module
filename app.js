f587dab31241aaed02f89c2bcd235f7e3d5a2407
Dhruv.tech <dhruvmalik@live.com>
2017-06-24 14:58:14 +0530

    Added Template Engine & EJS support.

--- app.js
+++ app.js
@@ -7,3 +7,11 @@ const routes = require('./routes/core');
 app.listen(process.env.port || 3030);
 
 app.use('/api', routes);
+app.use('/static', express.static('static'));
+app.set('view engine', 'ejs');
+
+//Renders for Demo Application
+
+app.get('/demo', function(req, res){
+    res.render('/demo/index');
+})
